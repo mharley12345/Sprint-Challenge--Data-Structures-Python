532d711e7799a554f6086ee9f9396b2d206afe30
Mike Harley <mharley12345@gmail.com>
2020-01-31 08:09:13 -0800

    ring_buffer done

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,14 +8,46 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        #Add new nodes to buffer
+        #Nodes cannot exceed capacity, so size of DDLs must be at capacity
+        
+
+        #Do this when Buffer is under limit
+
+        if self.storage.length < self.capacity:
+        #Check if head is empty to start
+            if self.storage.head == None:
+                self.storage.add_to_head(item)
+                self.current = self.storage.head #To track current node
+            else:
+            #Add to tail otherwise
+                self.storage.add_to_tail(item)
+                self.current = self.storage.head
+        else:
+        #Buffer will become full, need to add items from oldest
+        #After it will start adding items at the head (oldest items start)
+            if self.current.next == None: #when last item is reached again
+
+                #Update the last item with new item and move pointer back to head, start the process again
+                self.current.value = item
+                self.current = self.storage.head
+                #update position of self.current to the head
+            else:#
+                self.current.value = item #replace the value and move to next
+                #move the current to next node
+                self.current = self.current.next
+        #Storage capacity will be reached then then will 
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
-
+        #Start at head and append until current pointer has nothing 
+        current = self.storage.head
+        while (current != None):
+            list_buffer_contents.append(current.value)
+            current = current.next
         return list_buffer_contents
 
 # ----------------Stretch Goal-------------------
